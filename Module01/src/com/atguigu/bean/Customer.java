f25e01c170cae28905b7f1bd73082ace6d3159cb
duanqifeng <18865380573@163.com>
2020-05-30 22:45:21 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,26 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+//这是单行注释。
+public class Customer {
+
+    public static void main(String[] args){
+        System.out.println("helloworld!!!");
+    }
+
+    ArrayList list = new ArrayList();
+
+    public ArrayList getList() {
+        return list;
+    }
+
+    public static void main(String[] args) {
+        main();
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Module01/src/com/atguigu/bean/Hello.java
@@ -0,0 +1,9 @@
+package com.atguigu.bean;
+
+/**
+ * @author WHT
+ * @create 2020-05-29 16:41
+ */
+public class Hello {
+    
+}
